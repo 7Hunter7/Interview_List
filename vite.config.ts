5ef85e7f41da9b881bef68a423f071a6dfbf345b
Hunter <i.am.hunter.777.ih@gmail.com>
2025-03-02 21:03:39 +0300

    Updated 'vite.config.ts' - Installed 'ite-plugin-html' and 'vite-plugin-vue-devtools'

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { fileURLToPath, URL } from "node:url";
-
+import { createHtmlPlugin } from "vite-plugin-html";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueDevTools(), createHtmlPlugin({})],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
