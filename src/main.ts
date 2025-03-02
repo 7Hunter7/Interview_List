2c92d92f4f11fcd29f470d238e3240b24c6c2a6d
Hunter <i.am.hunter.777.ih@gmail.com>
2025-03-02 08:24:33 +0300

    Git inited

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import "./assets/main.css";
+import "primevue/resources/theme/saga-blue/theme.css";
+import "primeicons/primeicons.css";
+
+import App from "./App.vue";
+import router from "./router";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import PrimeVue from "primevue/config";
+
+const app = createApp(App);
+const pinia = createPinia();
+
+app.use(pinia);
+app.use(router);
+app.use(PrimeVue);
+
+app.mount("#app");
---
+++ src/stores/index.ts
@@ -0,0 +1,13 @@
+import { defineStore } from "pinia";
+
+export const useStore = defineStore("counter", {
+  state: () => {
+    return { count: 0 };
+  },
+
+  actions: {
+    increment() {
+      this.count++;
+    },
+  },
+});
