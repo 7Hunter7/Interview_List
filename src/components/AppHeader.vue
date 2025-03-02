d58f1ca7e48f7bf32497ee29b709767d57b270e4
Hunter <i.am.hunter.777.ih@gmail.com>
2025-03-02 08:40:51 +0300

    Added new component 'AppHeader'

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const items = ref([
+  {
+    label: "Авторизация",
+    icon: "pi pi-user",
+    path: "/auth",
+  },
+  {
+    label: "Добавить",
+    icon: "pi pi-plus",
+    path: "/",
+  },
+  {
+    label: "Список собеседований",
+    icon: "pi pi-list",
+    path: "/list",
+  },
+  {
+    label: "Статистика",
+    icon: "pi pi-chast-pie",
+    path: "/statistic",
+  },
+]);
+</script>
+
+<template>
+  <app-menubar :model="items" class="menu"> </app-menubar>
+</template>
+
+<style scoped>
+.menu {
+}
+</style>
