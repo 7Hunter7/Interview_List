1838ea50645525f31b50524bdb0e0ddd7da84159
Hunter <i.am.hunter.777.ih@gmail.com>
2025-03-03 18:21:22 +0300

    Updated  'App' - Added import and usage 'getAuth'and 'onAuthStateChanged' from 'firebase'. Created 'onMounted()' with 'onAuthStateChanged'

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import AppHeder from "@/components/AppHeader.vue";
+import { useUserStore } from "@/stores/user";
+
+const userStore = useUserStore();
+
+onMounted(() => {
+  onAuthStateChanged(getAuth(), (user) => {
+    userStore.userId = user ? user.uid : ""; // Если пользователь авторизован, то записываем его id в хранилище
+  });
+});
 </script>
 
 <template>
