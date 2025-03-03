df72956b2e37c8c7aa9ad530b63716750928230d
Hunter <i.am.hunter.777.ih@gmail.com>
2025-03-03 16:28:44 +0300

    Updated 'main.ts' - Added import and usage 'firebaseConfig'

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,20 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import PrimeVue from "primevue/config";
 import Menubar from "primevue/menubar";
+import { initializeApp } from "firebase/app";
+
+// Web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAZXrBVCAvd7-hO5HmLGK7irbyXQuUIQd4",
+  authDomain: "interview-list-87bd1.firebaseapp.com",
+  projectId: "interview-list-87bd1",
+  storageBucket: "interview-list-87bd1.firebasestorage.app",
+  messagingSenderId: "182458649351",
+  appId: "1:182458649351:web:0e21ac695d542622b013a2",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 const app = createApp(App);
 const pinia = createPinia();
