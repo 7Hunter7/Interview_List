72ac133b7f729974580e5174ae45be1755d89793
Hunter <i.am.hunter.777.ih@gmail.com>
2025-03-04 08:10:47 +0300

    Updated 'store/user.ts

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const useUserStore = defineStore("user", () => {
-  const userId = ref<string>("Ivan");
+  const userId = ref<string>("");
 
   return { userId };
 });
