2e0bc3c2a17e90a1e2607f6f5c69dd6d69d8c7c3
Hunter <i.am.hunter.777.ih@gmail.com>
2025-03-05 09:25:06 +0300

    Updated 'shims-vue.d.ts'

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,7 +1,2 @@
 declare module "*.vue";
-declare module "@/store/user";
-declare module "@/interfaces";
-declare module "@/components";
 declare module "uuid";
-
-declare module "primevue/textaria";
